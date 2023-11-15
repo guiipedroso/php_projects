b26cd496c461f26ca4591c3d9c1cf4aa362ddcb6
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2023-11-15 20:53:18 -0300

    variable

--- php_01/variable.php
+++ php_01/variable.php
@@ -1,1 +1,14 @@
-Opa <?php echo 'Guilherme Pedroso'; ?>
+<?php
+
+$x = 10;
+$nome = "Guilherme"; //string
+$sobrenome = "Pedroso"; //string 
+$altura = 1.79; // inteiro 
+$status = true; //boolean
+$vazia = null; //vazia
+
+//echo $x + 2;
+echo $nome.' '.$sobrenome; //concatenação
+
+?>
+
