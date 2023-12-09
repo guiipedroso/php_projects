07e32c50db55ce97a5379fcd596bc1c0485403e6
Guilherme Pedroso <guilhermepedrosogg@gmail.com>
2023-12-08 23:48:11 -0300

    Simple CRUD

--- homework_2/conexao.php
+++ homework_2/conexao.php
@@ -7,7 +7,6 @@ if (!$conn) {
     die("Erro na conexão com o banco de dados: " . $conn->lastErrorMsg());
 }
 
-// Criação da tabela se não existir
 $query = "CREATE TABLE IF NOT EXISTS atividades (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     descricao TEXT NOT NULL,
